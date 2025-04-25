39dda71fa7c96bcef12c586cac73843d78727e4b
ama1t <amalmohan3443@gmail.com>
2025-04-25 09:13:26 +0000

    add delte button to remove todo items

--- ToDoApplication/app.js
+++ ToDoApplication/app.js
@@ -10,12 +10,26 @@ function addTodo() {
   }
 }
 
+function removeTodo(index) {
+  todos.splice(index, 1);
+  renderTodos();
+}
+
 function renderTodos() {
   const list = document.getElementById("todoList");
   list.innerHTML = "";
-  todos.forEach((todo) => {
+  todos.forEach((todo, index) => {
     const listItem = document.createElement("li");
     listItem.textContent = todo;
+    // Add a delete button to each todo item
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.style.color = "red";
+    deleteButton.onclick = function () {
+      removeTodo(index);
+    };
+    listItem.appendChild(deleteButton);
     list.appendChild(listItem);
   });
 }
