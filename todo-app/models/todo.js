5d526932610872404ea9bae44006b070c6809361
ama1t <amalmohan3443@gmail.com>
2025-05-24 14:57:13 +0000

    initial rendering

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -19,6 +19,10 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
 
+    static getTodos() {
+      return this.findAll();
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
     }
